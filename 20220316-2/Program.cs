4aeead89461ef0fa45ce4e475ab75c4a25a3389f
S1mpleQQ <c110196116@nkust.edu.tw>
2022-03-16 16:49:34 +0800

    加入專案檔案。

---
+++ 20220316-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20220316_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            char op;
+            Console.Write("Number1:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number2:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Operator(+,-,*,/):");
+            op = Convert.ToChar(Console.ReadLine());
+            
+            switch(op)
+            {
+                case '+':
+                    Console.WriteLine("{0}=", a + b);
+                    break;
+                case '-':
+                    Console.WriteLine("{0}=", a + b);
+                    break;
+                case '*':
+                    Console.WriteLine("{0}=", a + b);
+                    break;
+                case '/':
+                    Console.WriteLine("{0}=", a + b);
+                    break;
+                default:
+                    Console.WriteLine("你不是機器人無法執行");
+
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
